0230c2655f0ab6d1c80231e3ef5ea1f31a1c8951
MACantara <cantara.michaelangelo@gmail.com>
2026-01-25 13:49:42 +0800

    Increase JSON and URL-encoded body size limits to 50mb in middleware

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const limiter = rateLimit({
 });
 
 // Global middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cors());
 app.use(limiter);
 
