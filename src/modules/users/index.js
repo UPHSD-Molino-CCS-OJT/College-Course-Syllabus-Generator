612c414909b19691c5adc980e841b7104bbff552
MACantara <cantara.michaelangelo@gmail.com>
2026-01-25 13:48:47 +0800

    Remove the old user module

--- src/modules/users/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userController = require("./controller");
-
-router.post("/add-new-user", userController.addUser);
-router.get("/", userController.getAllUsers);
-router.route("/:id").patch(userController.updateById);
-
-module.exports = router;
