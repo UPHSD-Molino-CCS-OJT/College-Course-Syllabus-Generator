eb9b25422e5dee5d56987fb240bc7e5a111d629f
MACantara <cantara.michaelangelo@gmail.com>
2026-03-10 18:27:16 +0800

    Remove files

--- src/modules/ai/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const aiController = require("./controller");
-
-router.post("/generate-syllabus", aiController.generateSyllabus);
-
-module.exports = router;
